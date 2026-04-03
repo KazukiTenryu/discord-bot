eadbccd0d8f48e2b4e50d3048f983392c97a1285
Kazuki Tenryu <Kazuki.Tenryu@proton.me>
2026-04-03 22:08:10 +0100

    feat: add ready listener to Main

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -4,8 +4,14 @@ import java.util.EnumSet;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jspecify.annotations.NonNull;
 
 import bot.config.Config;
 import bot.config.ConfigLoader;
@@ -27,16 +33,21 @@ public class Main {
 
             JDA jda = JDABuilder.createLight(config.botToken(), EnumSet.allOf(GatewayIntent.class))
                     .addEventListeners(new GlobalEventListener(config, database, slashCommandRepository))
+                    .addEventListeners(new ListenerAdapter() {
+                        private static final Logger LOGGER = LogManager.getLogger("Main#ReadyListener");
+
+                        @Override
+                        public void onReady(@NonNull ReadyEvent event) {
+                            LOGGER.info(
+                                    "Bot is ready! Logged in as {}",
+                                    event.getJDA().getSelfUser().getName());
+                        }
+                    })
                     .build();
 
             CommandListUpdateAction commands = jda.updateCommands();
-
             slashCommandRepository.getCommands().forEach(slashCommand -> commands.addCommands(slashCommand.getData()));
-
             commands.queue();
-
-            System.out.println("Bot has started successfully");
-
         } catch (Exception e) {
             System.err.println("Failed to start application: " + e.getMessage());
         }
